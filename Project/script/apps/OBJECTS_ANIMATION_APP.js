b5d7a00989fd94f6c81d48efd0b42a1dd687817d
CarnivoreOC <odahumlung@gmail.com>
2017-05-30 19:47:02 +0200

    Added wave animation

--- Project/script/apps/OBJECTS_ANIMATION_APP.js
+++ Project/script/apps/OBJECTS_ANIMATION_APP.js
@@ -6,7 +6,20 @@ var OBJECTS_ANIMATION_APP = {
         
         var shakeMask = $("#fireMask").click(function(){
             $(this).effect("shake", "slow");
-        })
+            
+        }) // end shakemask
+        
+        
+        var moveWave = $("#waterWave").click(function(){
+            $(this).animate(
+                {
+                    left: "100px",
+                    width: "250px"
+                }
+            );
+        }) // end moveWave
+        
+        var loopWave = setInterval(function(){ moveWave() }, 300);
         
     }//end init
     
