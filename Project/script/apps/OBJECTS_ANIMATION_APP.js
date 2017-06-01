de51afb2ec858914f3389cf021785c74fab4d016
Oda Humlung <oda.humlung@hotmail.com>
2017-06-01 17:00:46 +0200

    Fixed bugs

--- Project/script/apps/OBJECTS_ANIMATION_APP.js
+++ Project/script/apps/OBJECTS_ANIMATION_APP.js
@@ -1,79 +1,38 @@
 var OBJECTS_ANIMATION_APP = {
     init: function () {
             var OP = OBJECTS_ANIMATION_APP;
-            
-<<<<<<< Updated upstream
-        }) // end shakemask
-        
-        
-        var moveWave = $("#waterWave").click(function(){
-            $(this).animate(
-                {
-                    left: "-200px"
-                });
-            
-        }) // end moveWave
-        
-        var volcaneSmoke = $("#fireVolcano").click(function(){
-            $(this).fadeOut(2000);
-            $(this).fadeIn(2000);
-            
-        }) //end volcaneSmoke
-    
-        // rain function
-        var nbDrop = 500;
-        
-        var getRandomRange = function(minNum, maxNum){
-            
-            return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
-        
-        }// end of randomrange
-    
-        var createRain = function(){
-            var running = true;
-            var rain = $(".rain");
-            
-                for (var i = 1; i < nbDrop; i++) {
-                    var dropLeft = getRandomRange(0, 1600);
-                    var dropTop = getRandomRange(-1000, 1400);
-            if (running == true){
-                rain.append('<div class="drop" id="drop' + i + '"></div>')
-                $("#drop" + i).css("left", dropLeft);
-                $("#drop" + i).css("top", dropTop);
-                running = true;
-                }else{
-                    $((".drop")).remove();
-                    running = false;
-                }
-            }
-        }// end of create rain
-        
-        var makeItRain = $("#airRain").click(function(){
-            $(document.getElementById("airRain")).toggleClass("raining");
-            if ($(this).hasClass("raining")){
-                createRain();
-            }else {
-                createRain();
-            }
-
-                
-        })
-        
-        
-        var createLightning = $("#airSky").click(function(){
-            $(document.getElementById("airLightning")).toggleClass("flashit");
-        })
-        
-        //end rain
-        
-        
-        //var loopWave = setInterval(function(){ moveWave() }, 300);
-        
-    }//end init
-    
-    
-}// end app
-=======
+            var makeItRain = $("#airRain").click(function () {
+                    $(document.getElementById("airRain")).toggleClass("raining");
+                    if ($(this).hasClass("raining")) {
+                        createRain();
+                    }
+                    else {
+                        createRain();
+                    }
+                }) //End makeItRain
+            var nbDrop = 500;
+            var createRain = function () {
+                    var running = true;
+                    var rain = $(".rain");
+                    for (var i = 1; i < nbDrop; i++) {
+                        var dropLeft = getRandomRange(0, 1600);
+                        var dropTop = getRandomRange(-1000, 1400);
+                        if (running == true) {
+                            rain.append('<div class="drop" id="drop' + i + '"></div>')
+                            $("#drop" + i).css("left", dropLeft);
+                            $("#drop" + i).css("top", dropTop);
+                            running = true;
+                        }
+                        else {
+                            $((".drop")).remove();
+                            running = false;
+                        }
+                    }
+                } // end of create rain
+            var createLightning = $("#airSky").click(function () {
+                    $(document.getElementById("airLightning")).toggleClass("flashit");
+                }) // End createLightning
+                //var loopWave = setInterval(function(){ moveWave() }, 300);
             var shakeMask = $("#earthSnake").click(function () {
                     $(this).effect("shake", "slow");
                 }) // end shakemask
@@ -92,22 +51,9 @@ var OBJECTS_ANIMATION_APP = {
                         , total: 40
                     });
                 }) //end campfireDerbis
-            var nbDrop = 500;
             var getRandomRange = function (minNum, maxNum) {
-                return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
-            }
-            var createRain = function () {
-                var rain = $(".rain");
-                for (var i = 1; i < nbDrop; i++) {
-                    var dropLeft = getRandomRange(0, 1600);
-                    var dropTop = getRandomRange(-1000, 1400);
-                    rain.append('<div class="drop" id="drop' + i + '"></div>')
-                    $("#drop" + i).css("left", dropLeft);
-                    $("#drop" + i).css("top", dropTop);
-                }
-            }
-            createRain();
-            //var loopWave = setInterval(function(){ moveWave() }, 300);
+                    return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+                } // End randomrange
+                //var loopWave = setInterval(function(){ moveWave() }, 300);
         } //end init
 }
->>>>>>> Stashed changes
